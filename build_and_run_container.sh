e46f5f49c60fdc74bacc290ba369fe798cb3894c
Dennis Priskorn <egils@riseup.net>
2024-02-15 19:14:55 +0100

    fix: Simplify bash script

--- build_and_run_container.sh
+++ build_and_run_container.sh
@@ -1,3 +1,4 @@
 # Build and run the container
 # --rm means don't store changes
-touch /tmp/gunicorn_ipc.sock ; docker build -t topiccurator .; docker run --rm -p 5000:5000 topiccurator
+# touch /tmp/gunicorn_ipc.sock &&
+docker build -t topiccurator . && docker run --rm -p 5000:5000 topiccurator
